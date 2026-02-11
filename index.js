323f8b2f701b4cb99ccc67605a35de26ca9a606e
Archayne <hargrovea31@gmail.com>
2026-02-11 11:50:32 -0500

    created basic starting pages and added ejs

---
+++ index.js
@@ -0,0 +1,4 @@
+function main(){
+    console.log("Hello World!");
+    console.log("Here is a changed file");
+}
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express() //Calling express as a function sets up server
+
+app.get('/',(req, res)=>{
+    console.log('Here');
+    res.render('index');
+});
+app.get('/potato',(req, res)=>{
+    res.send('<p>Here are your potatoes</p>')
+});
+app.get('/status', (req, res)=>{
+    res.status(500).send('Hi');
+});
+app.listen(3030); //Tell our app to listen for requests
